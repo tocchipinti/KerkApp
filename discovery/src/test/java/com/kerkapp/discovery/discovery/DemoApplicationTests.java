920fa401caafe0ead3934c9830ec0cf8c3c77dc5
Tim Occhipinti <t.occhipinti@outlook.com>
2020-08-15 15:08:07 +0200

    Initial commit of discovery

---
+++ discovery/src/test/java/com/kerkapp/discovery/discovery/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kerkapp.discovery.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

8dc159a83466cce64b8b3591d0894eb527252054
Tim Occhipinti <t.occhipinti@outlook.com>
2020-12-23 09:17:42 +0100

    Spring config server
    Config server added
    added config for registry service

---
+++ config-server/src/test/java/com/kerkapp/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kerkapp.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
